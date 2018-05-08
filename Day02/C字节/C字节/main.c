2778cae42c069b23567698748250bee4ef0c6f4f
oneyaers <970771737@qq.com>
2018-05-08 11:55:08 +0800

    Mac测试提交

---
+++ Day02/C字节/C字节/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C字节
+//
+//  Created by Neworigin on 18/5/8.
+//  Copyright © 2018年 Neworigin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ Day02/Day2字节大小/Day2字节大小/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Day2字节大小
+//
+//  Created by Neworigin on 18/5/8.
+//  Copyright © 2018年 Neworigin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
